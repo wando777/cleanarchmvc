e5b2d65c1ea4fdbc1d888da8c1716a6bf67caaa1
wando777 <wanderson.eq2011@gmail.com>
2023-10-15 09:50:40 -0300

    create category interface and service in Application layer

---
+++ CleanArchMvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchMvc.Application.DTOs;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategoriesAsync();
+        Task<CategoryDTO> GetByID(int? id);
+        Task AddCategoryAsync(CategoryDTO categoryDTO);
+        Task UpdateCategoryAsync(CategoryDTO categoryDTO);
+        Task RemoveCategoryAsync(int? id);
+    }
+}
---
+++ CleanArchMvc.Application/Services/CategoryService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public Task AddCategoryAsync(CategoryDTO categoryDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CategoryDTO> GetByID(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CategoryDTO>> GetCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveCategoryAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(CategoryDTO categoryDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
